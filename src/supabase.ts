c397af536a6c1c4a14915740ed920410f726b738
panoskoul108 <235298513+panoskoul108@users.noreply.github.com>
2026-08-18 01:06:47 +0300

    Fix Supabase URL format
    Updated Supabase URL to remove unnecessary path.

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Βάλε τα δικά σου κλειδιά εδώ μέσα στα αυτάκια
-const supabaseUrl = 'https://jfcdannwntmejfiueolg.supabase.co/rest/v1/';
+const supabaseUrl = 'https://jfcdannwntmejfiueolg.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmY2Rhbm53bnRtZWpmaXVlb2xnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY5NjYwMTQsImV4cCI6MjEwMjU0MjAxNH0.8twZxpHewl0XXvOQArfW5jjcmPZy8as50MeCwJbxL4g';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
